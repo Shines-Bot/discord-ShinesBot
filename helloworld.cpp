b70537faca6cd102faa3509ab469a3b7729e7aa2
Pythonee <125912730+TuberAsk@users.noreply.github.com>
2023-05-07 22:05:40 +0200

    Update and rename helloworld.py to helloworld.cpp
    Just a starter code for c++ users!

---
+++ helloworld.cpp
@@ -0,0 +1,16 @@
+# This is just a little starter script for C++ starters!
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    int test = 45;
+    int poo = 23;
+    int answer = poo * test;
+    std::cout << answer;
+    std::string ask = "\n\ncool";
+    std::cout << ask;
+    std::cin.get();
+}
--- helloworld.py
+++
@@ -1,3 +0,0 @@
-# This is just a little starter script!
-
-print("Hello World!")
