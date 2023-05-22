299b55c9e93be406a440ae5bf6761a0159ceb9b9
Pythonee <125912730+TuberAsk@users.noreply.github.com>
2023-05-22 22:45:12 +0200

    Create pip.py

---
+++ pip.py
@@ -0,0 +1,5 @@
+# If your pip doesn't work I would recommend trying this!
+
+py -3 -m pip install -U thelibrarytoinstall
+
+# I hope this helps also shout out to the discord.py library on python.org for showing me this method as my pip didn't work either!
